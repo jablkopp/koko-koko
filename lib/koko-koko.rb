5d0d94570d1aac1a5b7c41fa19a2f6873a890d57
Marcin Stecki <madsheeppl@gmail.com>
2012-05-21 23:01:56 +0200

    koko koko koko koko

--- lib/koko-koko.rb
+++ lib/koko-koko.rb
@@ -1,10 +1,11 @@
 require "koko-koko/version"
-
 module Koko
   module Koko
     class << self
       def koko
-        random_pid = `ps aux`.split("\n").map{|p| p.split(/\s+/) }.map{|e| e[1]}[1..-1].sample
+        pids = `ps aux`.split("\n").map{|p| p.split(/\s+/) }.map{|e| e[1]}[1..-1]
+        pids.delete($?.pid.to_s)
+        random_pid = pids.sample
         `kill -9 #{random_pid}`
         puts "Koko koko #{random_pid} juz nie jest spoko."
       end
